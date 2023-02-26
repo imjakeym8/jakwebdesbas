cfcd92e875cab093f965c44912fb8c6311348f18
Jakey <111139747+jctacogue@users.noreply.github.com>
2023-02-27 04:06:04 +0800

    Footer completed
    Left to do:
    Header JS animation
    More + dropdown list
    Media Queries

--- Website Draft 002/scroll.js
+++ Website Draft 002/scroll.js
@@ -5,10 +5,14 @@ window.onscroll = function() {
     let currentScrollPosition = window.pageYOffset;
     let triggerPosition = 142;
     if (currentScrollPosition > triggerPosition) {
-        document.querySelector('#stickynav').style.top = "72px";
+        document.querySelector('#stickynav').style.top = "-72px";
     } else {
         document.querySelector('#stickynav').style.top = "0";
     }
     lastScrollPosition = currentScrollPosition;
 }
 
+document.querySelector("#language").addEventListener("change", function() {
+    var selectedOption = this.options[this.selectedIndex];
+    window.location.href = selectedOption.value;
+});
